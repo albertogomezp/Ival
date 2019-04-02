c1406691dda631d2cdae7079d0d48fa043ad4ece
Alberto <proyectos@albertogomp.es>
2019-04-02 12:20:37 +0200

    Added JS folder

---
+++ Resource/WebContent/js/scripts.js
@@ -0,0 +1,63 @@
+/**
+ * 
+ */ 
+function updateClock ( ) // A simple clock for the web.
+{
+  var currentTime = new Date ( );
+
+  var currentHours = currentTime.getHours ( );
+  var currentMinutes = currentTime.getMinutes ( );
+  var currentSeconds = currentTime.getSeconds ( );
+
+  // Pad the minutes and seconds with leading zeros, if required
+  currentMinutes = ( currentMinutes < 10 ? "0" : "" ) + currentMinutes;
+  currentSeconds = ( currentSeconds < 10 ? "0" : "" ) + currentSeconds;
+
+  // Choose either "AM" or "PM" as appropriate
+  var timeOfDay = ( currentHours < 12 ) ? "AM" : "PM";
+
+  // Convert the hours component to 12-hour format if needed
+  currentHours = ( currentHours > 12 ) ? currentHours - 12 : currentHours;
+
+  // Convert an hours component of "0" to "12"
+  currentHours = ( currentHours == 0 ) ? 12 : currentHours;
+
+  // Compose the string for display
+  var currentTimeString = currentHours + ":" + currentMinutes + ":" + currentSeconds + " " + timeOfDay;
+
+  // Update the time display
+  document.getElementById("clock").firstChild.nodeValue = currentTimeString;
+}
+function callJqueryAjax(action){
+	  //var action = $('#name').val();
+	  $.ajax(
+	    {
+	      url     : '/Core',
+	      method     : 'POST',
+	      data     : {action: action},
+	      success    : function(resultText){ $('#resultServlet').html(resultText); },
+	      error : function(jqXHR, exception){
+	        console.log('Error occured!!');
+	      }
+	    }
+	    );
+	  };
+  function moveUserData(action){
+	  console.log("init script");
+	  //var action = $('#name').val();
+	  var user = document.getElementById("Username").value;
+	  var password = document.getElementById("password").value;
+	  console.log(user);
+	  console.log(password);
+	  $.ajax(
+	    {
+	      url     : '/Core',
+	      method     : 'POST',
+	      data     : {action: action,"user":user,"password":password},
+	      success    : function(resultText){ $('#resultServlet').html(resultText); console.log("success"); },
+	      error : function(jqXHR, exception){
+	        console.log('Error occured!!');
+	      }
+	    }
+	    );
+	  };
