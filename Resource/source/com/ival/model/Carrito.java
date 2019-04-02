f660bb443d4427a02c14c88705eaa53075a6c1d8
Alberto <proyectos@albertogomp.es>
2019-04-02 09:47:57 +0200

    Added Carrito Class

---
+++ Resource/source/com/ival/model/Carrito.java
@@ -0,0 +1,40 @@
+package com.ival.model;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+/*
+ * @author: Alberto Gomez 
+ * @author: Ivan Perez
+ */
+
+@Entity
+@Table(name = "Carrito")
+public class Carrito  {
+	
+	@Id	
+	@Column(name = "CodCar")
+	private Integer CodCar;
+	@Column(name = "FechCar")
+	private Date FechCar;
+	@Column(name = "TotCar")
+	private float TotCar;
+	
+	public Carrito() {
+		super();
+	}
+	
+	public Carrito(Integer codCar, Date fechCar, float totCar) {
+		super();
+		CodCar = codCar;
+		FechCar = fechCar;
+		TotCar = totCar;
+	}
+	
+	
+}
